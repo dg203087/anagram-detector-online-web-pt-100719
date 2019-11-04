511031db07babfbdfa580511e84cba094e9bd117
Deanna Kelley <deanna.georges@gmail.com>
2019-11-04 01:03:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(array)
+    array.each do |word|
+      word.split("").sort 
+      if word == @word
+        return word
+      end
+    end
+  end 
+  
+end 
